52e74ba43b8d2d5dfb34ce13456e773be9e223fa
Kyohei Suzuki <42802497+broccoli-kyon@users.noreply.github.com>
2020-10-12 21:17:32 +0900

    Add files via upload

---
+++ js/back.js
@@ -0,0 +1,89 @@
+﻿"use strict"; 
+{
+  const menu = document.getElementById("menu");
+  
+  menu.addEventListener("click", () => {
+    const topMenu = document.getElementById("topMenu");
+    topMenu.classList.toggle("hidden");
+  });  
+  
+  
+  const about = document.getElementById("about");
+  
+  about.addEventListener("click", () => {
+    const aboutDeploy = document.getElementById("aboutDeploy");
+    aboutDeploy.classList.toggle("hidden");
+  });   
+  
+  const team = document.getElementById("team");
+  
+  team.addEventListener("click", () => {
+    const teamDeploy = document.getElementById("teamDeploy");
+    teamDeploy.classList.toggle("hidden");
+  }); 
+  
+  const house = document.getElementById("house");
+  
+  house.addEventListener("click", () => {
+    const houseDeploy = document.getElementById("houseDeploy");
+    houseDeploy.classList.toggle("hidden");
+  });
+  
+  const product = document.getElementById("product");
+  
+  product.addEventListener("click", () => {
+    const productDeploy = document.getElementById("productDeploy");
+    productDeploy.classList.toggle("hidden");
+  });
+   
+  
+  const shop = document.getElementById("shop");
+  
+  shop.addEventListener("click", () => {
+    const shopDeploy = document.getElementById("shopDeploy");
+    shopDeploy.classList.toggle("hidden");
+  });
+
+
+     
+     
+     
+     
+  //右下矢印クリックでページトップで戻る
+  const back = document.getElementById("back");
+
+  back.addEventListener("click", () => {
+    scrollTo(0, 0);
+  }); 
+  
+  
+
+  const deploy = document.querySelectorAll(".deploy");
+  
+  
+
+       
+	const bottomContents = document.getElementById("bottomContents");
+  
+  bottomContents.addEventListener("click", () => {
+    const bottomContentsDeploy = document.getElementById("bottomContentsDeploy");
+    bottomContentsDeploy.classList.toggle("hidden");
+  }); 
+         
+  
+  const bottomSupport = document.getElementById("bottomSupport");
+  
+  bottomSupport.addEventListener("click", () => {
+    const bottomSupportDeploy = document.getElementById("bottomSupportDeploy");
+    bottomSupportDeploy.classList.toggle("hidden");
+  });       
+  
+  
+  const bottomContact = document.getElementById("bottomContact");
+  
+  bottomContact.addEventListener("click", () => {
+    const bottomContactDeploy = document.getElementById("bottomContactDeploy");
+    bottomContactDeploy.classList.toggle("hidden");
+  });
+           
+}
---
+++ js/slide.js
@@ -0,0 +1,104 @@
+﻿"use strict"
+{
+  const images = [
+    "img/slide/music.gif",
+    "img/slide/PC.jpg",
+    "img/slide/pc-1.jpg",
+    "img/slide/tee.gif"
+  ];
+
+  const largeImg = [
+    "img/slide/music_large.gif",
+    "img/slide/SP.jpg",
+    "img/slide/SP-1.jpg",
+    "img/slide/tee_large.gif"
+  ];
+
+  //はじめに表示されている画像をあらわす
+  let  currentIndex = 0; 
+  let img;
+  
+  function checkWidth() {
+  	if(window.innerWidth >= 640) {
+  		img = images;
+  	} else {
+  		img = largeImg;
+  	}
+  
+  
+  }   
+  window.onload =  checkWidth(); 
+  
+  
+
+  const mainPic = document.getElementById("mainPic");
+ 
+  
+  window.addEventListener("resize", () => {
+  	     checkWidth()
+  });  
+  
+  
+  const list = document.querySelectorAll(".list");
+  function changeOpacity(currentIndex) {
+  	for(let i = 0; i < currentIndex; i++) {
+	  	
+  		if(i === currentIndex) {
+  			
+  			list[i].style.opacity = 1;
+  		} else {
+  			list[i].style.opacity = 0.5;
+  		}                       	
+  	}
+  } 
+  
+  console.log(list);
+
+  //次へボタンの実装
+  const next = document.getElementById("next");
+  next.addEventListener("click", () =>  {
+    
+    
+    let target = currentIndex ++;
+    
+    if (target === img.length) {
+      currentIndex = 0;
+      target = currentIndex;
+      mainPic.src = img[target];
+    }
+    mainPic.src = img[target];
+
+
+  });
+  
+
+    //前へボタンの実装
+  const prev = document.getElementById("prev");
+  prev.addEventListener("click", () =>  {
+
+    //前のサムネイルを現在のサムネから+1と表現している
+    let target = currentIndex --;
+
+
+    //最初のサムネまで行ったら最初に戻る処理
+    if (target < 0) {
+      currentIndex = img.length -1;
+      target = currentIndex;
+      mainPic.src = img[target];  
+      
+    }
+    mainPic.src = img[target];
+  });
+
+  let timeout;
+
+function count() {
+  next.click(); 
+   changeOpacity(currentIndex);
+   console.log(currentIndex);
+  timeout = setTimeout(count, 3000);
+}
+
+count(); 
+
+}
